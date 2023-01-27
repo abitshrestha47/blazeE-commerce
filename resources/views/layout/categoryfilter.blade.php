c767d23035015a57e6ae0f7628be1bd4029cdbd4
abitshrestha <abitshrestha2079@gmail.com>
2023-01-27 09:41:46 +0545

    categoryfilter

--- resources/views/layout/categoryfilter.blade.php
+++ resources/views/layout/categoryfilter.blade.php
@@ -4,7 +4,7 @@
     <div class="col-12 col-sm-6 col-lg-4 single_gallery_item wow fadeInUpBig" data-wow-delay="0.2s">
         <!-- Product Image -->
         <div class="product-img">
-            <img src="{{asset('/storage/' .$goods->photo)}}" alt="">
+            <img src="{{$goods->photo}}" alt="">
             <div class="product-quicview">
                 <a href="#" data-toggle="modal" data-target="#quickview"><i class="ti-plus"></i></a>
             </div>
@@ -13,6 +13,8 @@
         <div class="product-description">
             <h4 class="product-price">{{$goods->price}}</h4>
             <p>{{$goods->name}}</p>
+            <p>{{$goods->brand}}</p>
+            <p>{{$goods->category->categories}}</p>
             <!-- Add to Cart -->
             <a href="#" class="add-to-cart-btn">ADD TO CART</a>
         </div>
